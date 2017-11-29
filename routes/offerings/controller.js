7927deffd84ceb7129c3b81c394e4d1c2edc0cb1
Zohreh Sadeghi <zsadeghi@uw.edu>
2017-11-28 21:43:59 -0800

    Make `/offerings` endpoint RESTful
    This fixes #4

--- routes/offerings/controller.js
+++ routes/offerings/controller.js
@@ -6,17 +6,16 @@ const Adapter = Promise.promisifyAll(require('../../adapters/offerings'));
 
 exports.getOfferings = (req, res) => {
   Adapter.getOfferings(req.query, (err, rows) => {
-    res.json({
-      offerings: rows.map(offering => {
-        return {
-          id: offering.id,
-          name: offering.name,
-          description: offering.description,
-          type: offering.type,
-          ingredients: offering.ingredients.split(','),
-          tags: offering.tags.split(',')
-        }
-      })
-    });
+    res.json(rows.map(offering => {
+            return {
+                id: offering.id,
+                name: offering.name,
+                description: offering.description,
+                type: offering.type,
+                ingredients: offering.ingredients.split(','),
+                tags: offering.tags.split(',')
+            }
+        })
+    );
   });
 };
