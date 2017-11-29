8f52e475f3299ab2df28f659f82eabe602247cf4
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-11-28 22:07:00 -0800

    added error handler

--- routes/offerings/controller.js
+++ routes/offerings/controller.js
@@ -4,18 +4,19 @@ const Promise = require('bluebird');
 const _ = require('lodash');
 const Adapter = Promise.promisifyAll(require('../../adapters/offerings'));
 
-exports.getOfferings = (req, res) => {
+exports.getOfferingsBySearch = (req, res) => {
   Adapter.getOfferings(req.query, (err, rows) => {
+    if (err) return res.status(500).json({error: err});
+
     res.json(rows.map(offering => {
-            return {
-                id: offering.id,
-                name: offering.name,
-                description: offering.description,
-                type: offering.type,
-                ingredients: offering.ingredients.split(','),
-                tags: offering.tags.split(',')
-            }
-        })
-    );
+      return {
+        id: offering.id,
+        name: offering.name,
+        description: offering.description,
+        type: offering.type,
+        ingredients: offering.ingredients.split(','),
+        tags: offering.tags.split(',')
+      }
+    }));
   });
 };
