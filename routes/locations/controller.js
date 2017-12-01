6160ea83c05ba414a11b5dd98af763ada5076aa5
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-12-01 00:45:57 -0800

    fixed locations controller ternary operator

--- routes/locations/controller.js
+++ routes/locations/controller.js
@@ -6,13 +6,15 @@ exports.getLocations = (req, res, next) => {
   console.log('in controller');
   Adapter
     .getAllLocations()
-    .then(locations => locations.length ? res.status(404).json(locations) : res.json(locations))
+    .then(locations => locations.length ? res.json(locations) : res.status(404).json(locations))
     .catch(error => res.status(500).send(error));
 };
 
 exports.getLocationsById = (req, res, next) => {
+  console.log('req.params.id', req.params.id);
+
   Adapter
     .getLocation(req.params.id)
-    .then(locations => locations.length ? res.status(404).json(locations) : res.json(locations[0]))
+    .then(locations => locations.length ?  res.json(locations[0]) : res.status(404).json(locations))
     .catch(error => res.status(500).send(error));
 };
